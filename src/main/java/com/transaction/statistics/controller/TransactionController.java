ddcf07cb34c9f2f77e64ea8c24b002e4e2ec0f66
ayorinde.daniel <ayorinde.daniel@interswitchgroup.com>
2022-01-08 03:10:28 +0100

    added delete Transaction to controller class to delete all transactions.

--- src/main/java/com/transaction/statistics/controller/TransactionController.java
+++ src/main/java/com/transaction/statistics/controller/TransactionController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,5 +42,11 @@ public class TransactionController {
     public ResponseEntity<TransactionReport> getTransactionReport(){
         TransactionReport transactionReport = service.getTransactionReport();
         return new ResponseEntity<TransactionReport>(transactionReport, HttpStatus.OK);
+    }
+
+    @DeleteMapping ("/transactions")
+    public ResponseEntity<Void> deleteTransaction( @RequestBody Object object){
+        service.deleteTransaction(object);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 }
